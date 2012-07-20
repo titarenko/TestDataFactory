cc9f95c6e7d469637b96b28fb23988edc2cd655e
Constantin Titarenko <constantin.titarenko@gmail.com>
2012-07-20 22:50:51 +0300

    Updated version.

--- TestDataFactory/Properties/AssemblyInfo.cs
+++ TestDataFactory/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@
 [assembly: AssemblyTitle("TestDataFactory")]
 [assembly: AssemblyCopyright("Copyright (c) Constantin Titarenko, 2012")]
 
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.1.0")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
